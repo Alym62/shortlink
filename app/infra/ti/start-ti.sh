db60684716d570482006ac5c903fbaf9f7a172f8
Alym62 <alymeireles62@gmail.com>
2026-04-14 23:01:47 -0300

    feat: criação de GSI com projeção de email e senha

--- app/infra/ti/start-ti.sh
+++ app/infra/ti/start-ti.sh
@@ -1,17 +1,17 @@
 docker-compose up -d
 
-Write-Host "Waiting for 15 seconds"
-Start-Sleep -Seconds 15
-Write-Host "Done waiting"
+echo "Waiting for 15 seconds"
+sleep 15
+echo "Done waiting"
 
-aws --endpoint="http://localhost:4566" dynamodb create-table `
-  --region "us-east-1" `
-  --table-name "tb_users" `
-  --attribute-definitions `
-    "AttributeName=user_id,AttributeType=S" `
-    "AttributeName=email,AttributeType=S" `
-  --key-schema `
-    "AttributeName=user_id,KeyType=HASH" `
-  --provisioned-throughput `
-    "ReadCapacityUnits=5,WriteCapacityUnits=5" `
+aws --endpoint="http://localhost:4566" dynamodb create-table \
+  --region "us-east-1" \
+  --table-name "tb_users" \
+  --attribute-definitions \
+    "AttributeName=user_id,AttributeType=S" \
+    "AttributeName=email,AttributeType=S" \
+  --key-schema \
+    "AttributeName=user_id,KeyType=HASH" \
+  --provisioned-throughput \
+    "ReadCapacityUnits=5,WriteCapacityUnits=5" \
   --global-secondary-indexes file://gsi.json
