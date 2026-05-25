0a112c6f75316706dedd5fbb45b2df91c97a28ba
Alym62 <alymeireles62@gmail.com>
2026-05-25 14:30:49 -0300

    fix: correção na injeção seguindo as boas práticas de DI e Inversão de controle

--- app/src/main/java/com/github/shortlink/adapter/in/AuthController.java
+++ app/src/main/java/com/github/shortlink/adapter/in/AuthController.java
@@ -2,7 +2,7 @@ package com.github.shortlink.adapter.in;
 
 import com.github.shortlink.adapter.in.dto.LoginRequestDto;
 import com.github.shortlink.adapter.in.dto.LoginResponseDto;
-import com.github.shortlink.core.usecases.AuthenticateUseCase;
+import com.github.shortlink.core.port.in.AuthenticatePortIn;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/oauth")
 public class AuthController {
-    private final AuthenticateUseCase authenticateUseCase;
+    private final AuthenticatePortIn authenticatePortIn;
 
-    public AuthController(AuthenticateUseCase authenticateUseCase) {
-        this.authenticateUseCase = authenticateUseCase;
+    public AuthController(AuthenticatePortIn authenticatePortIn) {
+        this.authenticatePortIn = authenticatePortIn;
     }
 
     @PostMapping("/token")
     public ResponseEntity<LoginResponseDto> getToken(@RequestBody LoginRequestDto dto) {
-        final LoginResponseDto token = authenticateUseCase.execute(dto);
+        final LoginResponseDto token = authenticatePortIn.execute(dto);
         return ResponseEntity.ok().body(token);
     }
 }
