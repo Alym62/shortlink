3bebe08ab8688bb421cf9212b24ab16b342944a5
Alym62 <alymeireles62@gmail.com>
2026-05-26 21:38:47 -0300

    feat: script para criar tabela de links

--- app/infra/ti/start-ti.sh
+++ app/infra/ti/start-ti.sh
@@ -4,6 +4,8 @@ echo "Waiting for 15 seconds"
 sleep 15
 echo "Done waiting"
 
+# Create table users
+echo "Create table users"
 aws --endpoint="http://localhost:4566" dynamodb create-table \
   --region "us-east-1" \
   --table-name "tb_users" \
@@ -14,4 +16,18 @@ aws --endpoint="http://localhost:4566" dynamodb create-table \
     "AttributeName=user_id,KeyType=HASH" \
   --provisioned-throughput \
     "ReadCapacityUnits=5,WriteCapacityUnits=5" \
-  --global-secondary-indexes file://gsi.json
+  --global-secondary-indexes file://gsi-tb-users.json
+
+# Create table links
+echo "Create table links"
+aws --endpoint="http://localhost:4566" dynamodb create-table \
+  --region "us-east-1" \
+  --table-name "tb_links" \
+  --attribute-definitions \
+    "AttributeName=link_id,AttributeType=S" \
+    "AttributeName=user_id,AttributeType=S" \
+  --key-schema \
+    "AttributeName=link_id,KeyType=HASH" \
+  --provisioned-throughput \
+      ReadCapacityUnits=5,WriteCapacityUnits=5 \
+  --global-secondary-indexes file://gsi-tb-links.json
